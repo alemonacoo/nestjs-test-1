c1fbb6171b7688f54f3e4c141e72a612f9629ced
alemonacoo <ale.monaco@outlook.com>
2023-02-14 14:11:00 +0100

    adding user create + dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+
+//libreria per criptare passwords
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  signin() {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // generare password hash
+    const hash = await argon.hash(dto.password);
+
+    // saving user in DB
+    await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+      },
+    });
+
     return { msg: 'i am signed in' };
   }
-  signup() {
-    return { msg: 'i am signed up' };
-  }
+  // signin() {
+  //   return { msg: 'i am signed up' };
+  // }
 }
