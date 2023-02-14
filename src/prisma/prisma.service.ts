522b5dd64b68c9c2f4584cb6353f8fab12438729
alemonacoo <ale.monaco@outlook.com>
2023-02-14 16:05:57 +0100

    adding prisma db provider

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+require('dotenv').config();
 
+// provider per connettere DB
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    // chiama la funzione della classe originale
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+}
