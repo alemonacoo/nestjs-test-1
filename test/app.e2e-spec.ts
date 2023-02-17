0e044b10398e13cb2104e8c55b64a7d3f647b49a
alemonacoo <ale.monaco@outlook.com>
2023-02-17 17:52:03 +0100

    adding sign-up + sign-in e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('App e2e', () => {
   // Creo un modulo di test che importa l'intero AppModule
@@ -17,11 +20,117 @@ describe('App e2e', () => {
         whitelist: true, // rimuove in automatico tutti i campi non richiesti
       }),
     );
+
+    // inizializza app
+    await app.init();
+    await app.listen(3333);
+
+    // Svuota il Database da ogni user (e sotto-tabelle) ad ogni nuova inizializzazione
+    const prisma = app.get(PrismaService);
+    await prisma.user.deleteMany({});
+
+    // Imposta url di base
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
   afterAll(async () => {
     // chiudo app
     app.close();
   });
 
-  it.todo('should pass');
+  // TESTS
+  //
+  //
+  // AUTH
+  describe('Auth', () => {
+    //
+    // DTO
+    const dto: AuthDto = {
+      email: 'ale@gmail.com',
+      password: '123',
+      firstName: 'Alessandro',
+    };
+    //
+    // SIGN-UP
+    describe('Signup', () => {
+      it('should sign-up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+      it('should throw e. if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('should throw e. if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('should throw e. if password, email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400);
+      });
+    });
+    //
+    // SIGN-IN
+    describe('Signing', () => {
+      it('Should sign-in', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAT', 'access_token');
+      });
+    });
+  });
+
+  // USER
+  describe('User', () => {
+    describe('Get Current User', () => {
+      it('Should get current User', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAT}',
+          })
+          .expectStatus(200);
+      });
+    });
+    describe('Edit User', () => {});
+    describe('Edit User', () => {});
+  });
+
+  // Projects
+  describe('Projects', () => {
+    describe('Get Projects', () => {});
+    describe('Create Project', () => {});
+    describe('Get Project by id', () => {});
+    describe('Edit Project by id', () => {});
+    describe('Delete Project by id', () => {});
+  });
+
+  // Todo(s)
+  describe('To-Do(s)', () => {
+    describe('Get Projects', () => {});
+    describe('Create to-do', () => {});
+    describe('Get to-do by id', () => {});
+    describe('Edit to-do status by id', () => {});
+    describe('Delete to-do by id', () => {});
+  });
 });
