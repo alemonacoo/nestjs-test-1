aa614fa97c28ae2572af5191aab6aae7e400d48e
alemonacoo <ale.monaco@outlook.com>
2023-02-17 16:09:12 +0100

    fixing firstname-lastname requirements

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -2,18 +2,16 @@ import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsEmail()
-  @IsNotEmpty()
   email: string;
 
   @IsString()
-  @IsNotEmpty()
   password: string;
 
   @IsString()
   @IsOptional()
-  firstName: string;
+  firstName?: string;
 
   @IsString()
   @IsOptional()
-  lastName: string;
+  lastName?: string;
 }
