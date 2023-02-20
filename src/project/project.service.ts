e6568a517e7d25729e2065632c8986b3c39c2a93
alemonacoo <ale.monaco@outlook.com>
2023-02-20 16:34:06 +0100

    adding edit+delete crud(s)

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProjectDto, EditProjectDto } from './dto';
 
@@ -40,12 +43,60 @@ export class ProjectService {
   }
 
   // Edit project
-  editProjectById(
+  async editProjectById(
     userId: number,
     dto: EditProjectDto,
     projectId: number,
-  ) {}
+  ) {
+    // trovo progetto con id
+    const project = await this.prisma.project.findFirst({
+      where: {
+        id: projectId,
+      },
+    });
+
+    // Guard condition: dobbiamo essere sicuri che il progetto appartenga allo user
+    if (!project || project.userId !== userId) {
+      throw new ForbiddenException(
+        'Access to resource denied',
+      );
+    }
+
+    // ri-cerco progetto con id - poi update
+    return this.prisma.project.update({
+      where: {
+        id: projectId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
 
   // Delete project
-  deleteProjectById(userId: number, projectId: number) {}
+  async deleteProjectById(
+    userId: number,
+    projectId: number,
+  ) {
+    // trovo progetto con id
+    const project = await this.prisma.project.findFirst({
+      where: {
+        id: projectId,
+      },
+    });
+
+    // Guard condition: dobbiamo essere sicuri che il progetto appartenga allo user
+    if (!project || project.userId !== userId) {
+      throw new ForbiddenException(
+        'Access to resource denied',
+      );
+    }
+
+    // Cancello progetto
+    return this.prisma.project.delete({
+      where: {
+        id: projectId,
+      },
+    });
+  }
 }
